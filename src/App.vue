8666c6c1db2ecf3e6297976a40316e534191cfc7
lianhaibo <957377783@qq.com>
2022-06-17 14:18:34 +0800

    chore: add basical deps

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    asdasd
+  </div>
+</template>
+
+<script setup lang="ts">
+// defineOptions({
+//   name: 'App'
+// })
+
+</script>
+
+<style lang='scss' scoped>
+
+</style>
