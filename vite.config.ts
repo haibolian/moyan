a020014c5aae415336c7f10219efa07e05881021
lianhaibo <957377783@qq.com>
2022-06-17 14:24:52 +0800

    chore: add auto-import plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite'
 import {defineConfig} from 'vite'
 
 export default defineConfig({
   plugins: [
-    vue()
+    vue(),
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+      dts: 'src/auto-import.d.ts'
+    })
   ],
 })
