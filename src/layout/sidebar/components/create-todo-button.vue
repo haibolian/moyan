c6d2b0259364173ec807c0de62de96a8dfca00fc
lianhaibo <957377783@qq.com>
2022-06-25 17:42:13 +0800

    chore: create task panel

--- src/layout/sidebar/components/create-todo-button.vue
+++ src/layout/sidebar/components/create-todo-button.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-
+  <div class="bg-#fff rounded-13px p-20px pt-15px text-center">
+    <IconifyOnline size="4em" icon="carbon:add-filled" color="#2f82ff" />
+    <h3 class="color-#444444">Create New Task</h3>
+    <p class="pt-4px font-bold color-#ccc text-12px">
+      Or use 
+      <span class="color-orange">invita click</span>
+    </p>
   </div>
 </template>
 
