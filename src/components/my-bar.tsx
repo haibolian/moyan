a2c6c89c9ab4484c140ff1e9b040ed430c701030
lianhaibo <957377783@qq.com>
2022-06-25 17:02:06 +0800

    chore: optimize  bar

--- src/components/my-bar.tsx
+++ src/components/my-bar.tsx
@@ -1,6 +1,7 @@
 interface MyBarProps {
   height?: string
   color?: string
+  p?: string,
   px?: string
   py?: string
 }
@@ -9,11 +10,12 @@ const MyBar = function (props: MyBarProps) {
   const {
     height = '1px',
     color = '#d0d9e2',
+    p,
     px = '0',
     py = '15px'
   } = props
   return (
-    <div style={{ padding: py + ' ' + px }}>
+    <div style={{ padding: p ? p : py + ' ' + px }}>
       <div style={{ height, overflow: 'hidden', background: color }}></div>
     </div>
   )
