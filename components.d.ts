6d92ccb778ee4a1369e9679b56d8d1452374ff9d
lianhaibo <957377783@qq.com>
2022-06-26 12:16:28 +0800

    chore: use ElDividev

--- components.d.ts
+++ components.d.ts
@@ -9,6 +9,7 @@ declare module '@vue/runtime-core' {
     ElBadge: typeof import('element-plus/es')['ElBadge']
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCheckbox: typeof import('element-plus/es')['ElCheckbox']
+    ElDivider: typeof import('element-plus/es')['ElDivider']
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElInput: typeof import('element-plus/es')['ElInput']
