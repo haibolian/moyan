f7f27ab23dee33285b699a83aa557c37f07be046
lianhaibo <957377783@qq.com>
2022-07-13 23:34:41 +0800

    chore: rm propsInstance from editor-panel.ts and store core logic

--- src/components/editor-panel/editor-panel.ts
+++ src/components/editor-panel/editor-panel.ts
@@ -1,18 +1,5 @@
 import { ref, onMounted } from 'vue'
 import { useProp } from '@/hooks/use-prop'
 
-export const propsInstance = {
-  maxlength: {
-    type: [Number, String],
-    default: 1000
-  },
-  showWordLimit: {
-    type: Boolean,
-    default: true
-  },
-  placeholder: {
-    type: String,
-    default: '分享你的想法'
-  },
-}
+
 
