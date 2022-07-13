6b2767605f5f4f04aba09380904e643a86e13c7a
lianhaibo <957377783@qq.com>
2022-07-13 23:00:36 +0800

    chore: add test page

---
+++ src/router/modules/test.ts
@@ -0,0 +1,6 @@
+import Test from '@/views/test-page/index.vue'
+
+export default {
+  path: '/test',
+  component: Test
+}
---
+++ src/views/test-page/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    ad 
+  </div>
+</template>
+
+<script setup lang='ts'>
+
+</script>
+
+<script lang='ts'>
+export default {
+  name: 'index'
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
