c846bf519c1c6cf92b9c10b8eb1bffb6dd0c0fe1
lianhaibo <957377783@qq.com>
2022-08-20 00:24:30 +0800

    chore: add getJournal api

--- src/api/journal.ts
+++ src/api/journal.ts
@@ -4,10 +4,12 @@ const Api = {
   publish: '/journal/publish',
   saveDraft: '/journal/saveDraft',
   getDraft: '/journal/getDraft',
+  getJournal: '/journal/view',
   getList: '/journal/getAll',
   del: '/journal/del'
 }
 
 export const publish = (data: any) => http.post(Api.publish, data);
 export const getList = (params?: any) => http.get(Api.getList, params);
+export const getJournal = (params?: any) => http.get(Api.getJournal, params);
 export const del = (params?: any) => http.delete(Api.del, params);
