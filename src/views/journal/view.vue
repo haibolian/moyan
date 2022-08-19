1d125050663ba5d73eb42f7ccf72a28e0ce17f56
lianhaibo <957377783@qq.com>
2022-08-20 00:23:52 +0800

    feat: init journal view

--- src/views/journal/view.vue
+++ src/views/journal/view.vue
@@ -1,19 +1,60 @@
 <template>
-  <div>
-    asd
+  <div class="flex justify-center">
+    <!-- outline -->
+    <div class="">
+      
+    </div>
+    <!-- main -->
+    <div class="journal-view bg-#fff py-35px px-30px w-900px">
+      <h1 class="journal-title text-center"> {{ journalInfo.title }} </h1>
+      <div class="text-center">
+        <IconifyOnline align="unset" size="14px" icon="akar-icons:calendar"></IconifyOnline>
+        <span>{{ createInfo }}</span>
+      </div>
+      <div ref="containerRef" id="journal-container"/>
+    </div>
+    <!-- more -->
+    <div class="">
+      
+    </div>
   </div>
 </template>
 
 <script setup lang='ts'>
+import { getJournal } from '@/api/journal';
+import Vditor from 'vditor'
 
+const router = useRouter()
+const { id } = router.currentRoute.value.params
+
+const journalInfo: any = reactive({})
+const containerRef = ref();
+
+const createInfo = computed(() => {
+  return ` ${journalInfo.user?.nickname} · ${journalInfo.createdAt}`
+})
+
+onMounted(async () => {
+  const { success, message, data } = await getJournal(id);
+  Object.assign(journalInfo, data) 
+  Vditor.preview(containerRef.value, journalInfo.content)
+})
 </script>
 
 <script lang='ts'>
 export default {
-  name: 'view'
+  name: 'JournalView'
 }
 </script>
 
 <style lang='scss' scoped>
-
+.journal-view {
+  #journal-container {
+    padding:inherit;
+    margin: inherit;
+  }
+}
+ul {
+  list-style-type: unset;
+}
 </style>
