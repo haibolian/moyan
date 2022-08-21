c94fbada086e6da94db588934bea12381d373762
lianhaibo <957377783@qq.com>
2022-08-21 22:34:01 +0800

    style: journal list style

--- src/views/journal/index.vue
+++ src/views/journal/index.vue
@@ -6,8 +6,8 @@
     </div>
     <div class="journal-content p-15px bgcvar-my-bgc-normal rounded-2xl mt-20px">
       <el-table :data="journalList" size="" highlight-current-row>
-      <el-table-column prop='createdAt' :formatter="formatDate" label="时间"></el-table-column>
-        <el-table-column prop='title' label="标题">
+      <el-table-column prop='createdAt' :formatter="formatDate" label="时间" width="150"></el-table-column>
+        <el-table-column prop='title' label="标题" show-overflow-tooltip min-width="200">
           <template #="scope">
             <el-link
               class="colorvar-my-c-normal"
@@ -19,12 +19,12 @@
             </el-link>
           </template>
         </el-table-column>
-        <el-table-column prop='categoryName' label="分类">
+        <el-table-column prop='categoryName' label="分类" width="150">
           <template #="scope">
             <el-tag effect="dark" type="warning">{{scope.row.categoryName}}</el-tag>
           </template>
         </el-table-column>
-        <el-table-column label="操作">
+        <el-table-column label="操作" width="150">
           <template #="scope">
             <el-button type="primary" size="small" @click="handleEdit(scope.row)">编辑</el-button>
             <el-button type="danger" size="small" @click="handleDelete(scope.row)">删除</el-button>
