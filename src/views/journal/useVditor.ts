bcc6da7735d0e316b051dd53b12efbbe215bad7f
lianhaibo <957377783@qq.com>
2022-09-05 22:34:18 +0800

    feat: vditor upload pictures

--- src/views/journal/useVditor.ts
+++ src/views/journal/useVditor.ts
@@ -1,7 +1,7 @@
 import { ref } from 'vue';
 import Vditor from 'vditor';
 import 'vditor/dist/index.css';
-
+import { storageLocal } from '@/utils/storage';
 
 
 export const useVditor = () => {
@@ -10,6 +10,20 @@ export const useVditor = () => {
   const createVditor = (after: () => void) => {
     vditor.value = new Vditor('vditor', {
       after,
+      upload: {
+        url: 'http://localhost:3000/api/upload/vditor',
+        token: 'test',
+        fieldName: 'images',
+        headers: {
+          Authorization: `Bearer ${storageLocal.getItem('token')}`
+        },
+        accept: 'image/*',
+        success(editor, msg) {
+          const { data: { succMap } } = JSON.parse(msg)
+          const urls = succMap.map(([filename, url]) => `![${filename}](${url})`).join('\n')
+          vditor.value?.insertValue(urls)
+        }
+      }
     });
   }
 
